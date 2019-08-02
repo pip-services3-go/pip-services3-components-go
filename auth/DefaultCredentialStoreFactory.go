85c94191abd14cc30c14c52c0cf036a339b33340
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2019-08-01 18:59:57 -0700

    Added MemoryCredentialStore

--- auth/DefaultCredentialStoreFactory.go
+++ auth/DefaultCredentialStoreFactory.go
@@ -1,3 +1,16 @@
 package auth
 
-// Todo: Complete implementation
+import (
+	"github.com/pip-services3-go/pip-services3-commons-go/refer"
+	"github.com/pip-services3-go/pip-services3-components-go/build"
+)
+
+var MemoryCredentialStoreDescriptor = refer.NewDescriptor("pip-services", "credential-store", "memory", "*", "1.0")
+
+func NewCredentialStoreFactory() *build.Factory {
+	factory := build.NewFactory()
+
+	factory.RegisterType(MemoryCredentialStoreDescriptor, NewEmptyMemoryCredentialStore)
+
+	return factory
+}
--- auth/MemoryCredentialStore.go
+++ auth/MemoryCredentialStore.go
@@ -1,3 +1,58 @@
 package auth
 
-// Todo: Complete implementation
+import (
+	"github.com/pip-services3-go/pip-services3-commons-go/config"
+)
+
+type MemoryCredentialStore struct {
+	items map[string]*CredentialParams
+}
+
+func NewEmptyMemoryCredentialStore() *MemoryCredentialStore {
+	return &MemoryCredentialStore{
+		items: map[string]*CredentialParams{},
+	}
+}
+
+func NewMemoryCredentialStore(config *config.ConfigParams) *MemoryCredentialStore {
+	c := &MemoryCredentialStore{
+		items: map[string]*CredentialParams{},
+	}
+
+	c.Configure(config)
+
+	return c
+}
+
+func (c *MemoryCredentialStore) Configure(config *config.ConfigParams) {
+	c.ReadCredentials(config)
+}
+
+func (c *MemoryCredentialStore) ReadCredentials(config *config.ConfigParams) {
+	c.items = map[string]*CredentialParams{}
+
+	keys := config.Keys()
+	for _, key := range keys {
+		value := config.GetAsString(key)
+		credential := NewCredentialParamsFromString(value)
+		c.items[key] = credential
+	}
+}
+
+func (c *MemoryCredentialStore) Store(correlationId string, key string,
+	credential *CredentialParams) error {
+
+	if credential != nil {
+		c.items[key] = credential
+	} else {
+		delete(c.items, key)
+	}
+
+	return nil
+}
+
+func (c *MemoryCredentialStore) Lookup(correlationId string,
+	key string) (result *CredentialParams, err error) {
+	credential, _ := c.items[key]
+	return credential, nil
+}
