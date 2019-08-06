977ae1c8f64e518c647bae682d1f2f2af5093c85
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2019-08-06 16:11:57 -0700

    Added Shutdown component

---
+++ Component.go
@@ -0,0 +1,33 @@
+package components
+
+import (
+	"github.com/pip-services3-go/pip-services3-commons-go/config"
+	"github.com/pip-services3-go/pip-services3-commons-go/refer"
+	"github.com/pip-services3-go/pip-services3-components-go/count"
+	"github.com/pip-services3-go/pip-services3-components-go/log"
+)
+
+type Component struct {
+	dependencyResolver *refer.DependencyResolver
+	logger             *log.CompositeLogger
+	counters           *count.CompositeCounters
+}
+
+func InheritComponent() *Component {
+	return &Component{
+		dependencyResolver: refer.NewDependencyResolver(),
+		logger:             log.NewCompositeLogger(),
+		counters:           count.NewCompositeCounters(),
+	}
+}
+
+func (c *Component) Configure(config *config.ConfigMap) {
+	c.dependencyResolver.Configure(config)
+	c.logger.Configure(config)
+}
+
+func (c *Component) SetReferences(references refer.IReferences) {
+	c.dependencyResolver.SetReferences(references)
+	c.logger.SetReferences(references)
+	c.counters.SetReferences(references)
+}
--- index.go
+++ index.go
@@ -1,4 +1,4 @@
-package commons
+package components
 
 import (
 	_ "github.com/pip-services3-go/pip-services3-components-go/auth"
@@ -8,5 +8,7 @@ import (
 	_ "github.com/pip-services3-go/pip-services3-components-go/connect"
 	_ "github.com/pip-services3-go/pip-services3-components-go/count"
 	_ "github.com/pip-services3-go/pip-services3-components-go/info"
+	_ "github.com/pip-services3-go/pip-services3-components-go/lock"
 	_ "github.com/pip-services3-go/pip-services3-components-go/log"
+	_ "github.com/pip-services3-go/pip-services3-components-go/test"
 )
