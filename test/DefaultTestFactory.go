484f15b41c66a605c6f18b912317f0c1186ce33d
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2019-08-06 18:39:58 -0700

    Added DefaultTestFactory

---
+++ test/DefaultTestFactory.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+	"github.com/pip-services3-go/pip-services3-commons-go/refer"
+	"github.com/pip-services3-go/pip-services3-components-go/build"
+)
+
+var ShutdownDescriptor = refer.NewDescriptor("pip-services", "shutdown", "*", "*", "1.0")
+
+func NewDefaultLoggerFactory() *build.Factory {
+	factory := build.NewFactory()
+
+	factory.RegisterType(ShutdownDescriptor, NewShutdown)
+
+	return factory
+}
